ecce9ba755639fa75ff5deda9dfe35b841c797ab
Eduardo Costa <ecosta@tpk.com.br>
2012-08-25 23:57:10 -0300

    using / as index.html

--- ludum-dare-24/ludum-dare-24.js
+++ ludum-dare-24/ludum-dare-24.js
@@ -1,7 +1,10 @@
 var express = require("express");
+var fs      = require("fs");
 
 var app = express();
 app.use(express.cookieParser());
+
+app.get("/", passFile("web/index.html", "text/html"))
 
 app.all("*", function(req, resp) {
     resp.status(404);
@@ -12,3 +15,19 @@ app.all("*", function(req, resp) {
 app.listen(8080);
 
 console.error("Server started");
+
+function passFile(fn, type) {
+    return function(req, resp) {
+        fs.readFile(fn, function(err, data) {
+            if (err) {
+                resp.status(500);
+                resp.type("text/plain");
+                resp.send(err);
+            } else {
+                resp.status(200);
+                resp.type(type);
+                resp.send(data);
+            }
+        })
+    }
+}
